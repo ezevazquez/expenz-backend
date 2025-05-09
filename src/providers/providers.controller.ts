cd3c47476ad073a834f07a42544749bfcd951dfe
ezevazquez <ezejvazquez@hotmail.com>
2025-05-09 15:07:23 -0300

    add providers module

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Delete,
+  Body,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProvidersService } from './providers.service';
+import { CreateProviderDto } from './dto/create-provider.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('providers')
-export class ProvidersController {}
+export class ProvidersController {
+  constructor(private readonly service: ProvidersService) {}
+
+  @Roles('admin')
+  @Post()
+  create(@Body() dto: CreateProviderDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.service.findOne(id);
+  }
+
+  @Roles('admin')
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.service.remove(id);
+  }
+}
--- src/providers/providers.service.ts
+++ src/providers/providers.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Provider } from './provider.entity';
+import { CreateProviderDto } from './dto/create-provider.dto';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Injectable()
-export class ProvidersService {}
+export class ProvidersService {
+  constructor(
+    @InjectRepository(Provider)
+    private readonly repo: Repository<Provider>,
+  ) {}
+
+  @Roles('admin')
+  create(dto: CreateProviderDto) {
+    const provider = this.repo.create(dto);
+    return this.repo.save(provider);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  @Roles('admin')
+  remove(id: number) {
+    return this.repo.delete(id);
+  }
+}
