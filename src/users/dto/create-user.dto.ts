968e738fa0032b991b84ba94a4cce8033fb34de8
ezevazquez <ezejvazquez@hotmail.com>
2025-05-09 16:10:58 -0300

    fix: users creation default was 'user'

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,16 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, IsOptional, IsEnum } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   email: string;
 
   @IsString()
-  @MinLength(6)
   password: string;
 
   @IsString()
   name: string;
+
+  @IsOptional()
+  @IsEnum(['user', 'admin', 'superuser'])
+  role?: 'user' | 'admin' | 'superuser';
 }
